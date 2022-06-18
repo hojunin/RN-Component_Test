1130291e55e90a823ba48cb6806237bd949a285e
Nick <dlsghwns@naver.com>
2022-06-19 02:06:06 +0900

    페이져 뷰 테스트

---
+++ src/Screens/TabView/First.tsx
@@ -0,0 +1,47 @@
+import {View, Text, FlatList} from 'react-native';
+import React from 'react';
+import {useQuery} from 'react-query';
+
+const fetchData = () => {
+  return fetch(
+    'https://getcha-api-v7-prod.getcha.io/v7/community?page_per_count=30&page=1&type=11,12,13,26,27,28,29,30,32',
+  )
+    .then(res => res.json())
+    .catch(err => {
+      throw new Error(err);
+    });
+};
+
+const First = () => {
+  const {data, isError, isLoading} = useQuery('first', fetchData);
+
+  if (isError || isLoading) {
+    return (
+      <View>
+        <Text>뭔가 잘못됨</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={{flex: 1}}>
+      <FlatList
+        data={data.data.list.infos}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <View
+            style={{
+              flexDirection: 'row',
+              height: 52,
+              alignItems: 'center',
+              marginLeft: 20,
+            }}>
+            <Text>{`${item.id_contents} : ${item.title}`}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default First;
--- src/Screens/TabView/index.tsx
+++ src/Screens/TabView/index.tsx
@@ -1,15 +1,25 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import {PagerView} from 'react-native-pager-view';
+import First from './First';
+import Second from './Second';
+import Third from './Third';
+import Fourth from './Fourth';
 
 const TabViewScreen = () => {
   return (
     <PagerView style={styles.pagerView} initialPage={0}>
       <View key="1">
-        <Text>First page</Text>
+        <First key="1" />
       </View>
       <View key="2">
-        <Text>Second page</Text>
+        <Second />
+      </View>
+      <View key="3">
+        <Third />
+      </View>
+      <View key="4">
+        <Fourth />
       </View>
     </PagerView>
   );
